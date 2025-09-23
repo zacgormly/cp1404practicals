1ffd8523c608d96897e46a10f9ded09e0ce4c5a3
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-09-23 14:08:40 +1000

    Revert "Refactor password check program to use functions"
    This reverts commit 8bc7cedb6ece06e34d4980a590254ceb0705be6d.

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,23 +1,8 @@
 """Password Validator"""
 
-
-def main():
-    min_length = 3
-    symbol = "."
-    password = get_password(min_length)
-    print_asterisks(password, symbol)
-
-
-def print_asterisks(password, symbol="*"):
-    print(symbol * len(password))
-
-
-def get_password(min_length):
+min_length = 3
+password = input("Password: ")
+while len(password) < min_length:
+    print("Invalid password length.")
     password = input("Password: ")
-    while len(password) < min_length:
-        print("Invalid password length.")
-        password = input("Password: ")
-    return password
-
-
-main()
+print("*" * len(password))
