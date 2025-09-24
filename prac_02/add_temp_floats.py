7073f841e367225e90d8a5e74b98e0781c857c21
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-09-24 15:33:41 +1000

    Add temperature extension programs, add input and output .txt files

---
+++ prac_02/add_temp_floats.py
@@ -0,0 +1,17 @@
+"""Add Fahrenheit (floats) to temps_input.txt"""
+
+from random import uniform
+
+NUMBER_OF_TEMPERATURE_VALUES = 20
+TEMPERATURE_INPUT_FILE = "temps_input.txt"
+
+
+def main():
+    out_file = open(TEMPERATURE_INPUT_FILE, "w")
+    for i in range(NUMBER_OF_TEMPERATURE_VALUES):
+        fahrenheit = uniform(-200, 200)
+        print(fahrenheit, file=out_file)
+    out_file.close()
+
+
+main()
