41467a54879b5c56106793a309b90d9f89589af1
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-09-30 21:57:54 +1000

    Add random numbers file with answers (comments) and code

---
+++ prac_03/randoms.py
@@ -0,0 +1,20 @@
+"""Random Numbers"""
+
+import random
+
+# line 1 - print(random.randint(5, 20))
+# Tests: 11, 18, 11, 15, 8, 20, 11, 19
+# Smallest: 5
+# Largest: 20
+
+# line 2 - print(random.randrange(3, 10, 2))
+# Tests: 7, 3, 3, 3, 7, 5, 3, 7, 3, 3, 9, 3, 5, 7, 3, 3, 9, 9
+# Smallest: 3
+# Largest: 9
+
+# line 3 - print(random.uniform(2.5, 5.5))
+# Tests: 4.876412488691148, 4.635351814103025, 5.241661541658084, 3.585781940712501, 4.41453201578207
+# Smallest: 2.5
+# Largest: 5.5
+
+print(random.randint(1, 100))
