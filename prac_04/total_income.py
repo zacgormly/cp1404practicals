7578e3371866d385561f9b8c6ad2e49c5ccec4a5
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-10-08 14:48:14 +1000

    Refactor report printing function for SRP and add 'generate report' function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -8,21 +8,31 @@ def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
     number_of_months = int(input("How many months? "))
+    print()
 
     for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
 
-    print_report(incomes, number_of_months)
+    income_report = generate_report(incomes, number_of_months)
+    print_report(income_report)
 
 
-def print_report(incomes, number_of_months):
-    print("\nIncome Report\n-------------")
+def generate_report(incomes, number_of_months):
+    """Generate report values and store in tuple."""
+    report = []
     total = 0
     for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        report.append((month, income, total))
+    return report
+
+def print_report(income_report):
+    """Print report."""
+    print("\nIncome Report\n-------------")
+    for month, income, total in income_report:
+        print(f"Month {month:2} - Income: ${income:10.2f} \t\t\t Total: ${total:10.2f}")
 
 
 main()
