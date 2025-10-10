ea0a510c155c998f0addc3e87e3a77bda1b5d1dc
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-10-10 16:24:07 +1000

    Update with function docstrings

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,6 +8,7 @@ HIGH = 45
 
 
 def main():
+    """Program to generate an amount of picks based on user input."""
     number_of_picks = get_valid_number_of_picks()
     for i in range(number_of_picks):
         pick_numbers = []
@@ -20,6 +21,7 @@ def main():
 
 
 def get_new_random_number(pick_numbers):
+    """Get a random number between LOW and HIGH not already in its pick."""
     number = random.randint(LOW, HIGH)
     while number in pick_numbers:
         number = random.randint(LOW, HIGH)
@@ -27,6 +29,7 @@ def get_new_random_number(pick_numbers):
 
 
 def get_valid_number_of_picks():
+    """Get a valid positive integer number of picks."""
     number_of_picks = int(input("How many quick picks? "))
     while number_of_picks <= 0:
         print("Invalid number of picks.")
@@ -35,11 +38,13 @@ def get_valid_number_of_picks():
 
 
 def print_formatted_pick(ascending_numbers):
+    """Print pick numbers with neat format."""
     for number in ascending_numbers:
         print(f"{number:2}", end=' ')
 
 
 def order_pick_numbers(pick_numbers):
+    """Order pick numbers in ascending order."""
     ascending_numbers = [number for number in sorted(pick_numbers)]
     return ascending_numbers
 
