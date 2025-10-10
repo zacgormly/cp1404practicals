779b903f5fc11bd0c0228e71b70373c590d82caa
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-10-10 12:07:58 +1000

    Update with function docstrings

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -4,6 +4,7 @@ AMOUNT_OF_NUMBERS = 5
 
 
 def main():
+    """Program to determine first, last, smallest, largest and average of numbers."""
     numbers = get_numbers()
     print(f"The first number is {numbers[0]}")
     print(f"The last number is {numbers[-1]}")
@@ -14,6 +15,7 @@ def main():
 
 
 def get_numbers():
+    """Get numbers and store in list."""
     numbers = []
     for i in range(AMOUNT_OF_NUMBERS):
         number = int(input("Number: "))
@@ -22,6 +24,7 @@ def get_numbers():
 
 
 def calculate_average(numbers):
+    """Calculate average based on sum and length of list."""
     return sum(numbers) / len(numbers)
 
 
