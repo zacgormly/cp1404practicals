6009d3bd11fa4b7d75784340ae528b883329873c
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-10-14 12:36:04 +1000

    Remove 'reformat file' TODO

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,7 +4,6 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 MAX_STATE_CODE_LENGTH = 3
