017e8af0c0e8daa37a08a83e18d2a55bf75277a5
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-10-15 15:56:46 +1000

    Complete prac 5 Emails exercise

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,5 +1,30 @@
 """
 Emails
-Estimate: ?? minutes
-Actual:   ?? minutes
+Estimate: 30 minutes
+Actual:   24 minutes
 """
+
+
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = determine_name(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ")
+        if not (confirmation == "" or confirmation == "y"):
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+    print()
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def determine_name(email):
+    username = email.split('@')[0]
+    name_parts = username.split('.')
+    name = ' '.join(name_parts).title()
+    return name
+
+
+main()
