97447e9d3d203a285c62d5d8fd0fc09bb2199c30
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-10-29 01:54:23 +1000

    Update car class and test different cars

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,10 +9,19 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car(180, "My Car")
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
+    limo = Car(100, "Limo")
+    limo.add_fuel(20)
+    print(f"Limo has fuel: {limo.fuel}")
+    limo.drive(115)
+    print(limo)
+    taxi = Car(200, "Taxi")
+    print(taxi)
+    mystery_machine = Car(250, "The Mystery Machine")
+    print(mystery_machine)
 
 
 main()
