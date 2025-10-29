f831afe751d92cd33186492c2e4f74aae169e6f0
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-10-29 14:36:11 +1000

    Add programming languages programs

---
+++ prac_06/languages.py
@@ -0,0 +1,22 @@
+"""
+Languages Program
+Estimated Time: 30 minutes
+Actual Time:
+"""
+
+from programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
+print(ruby)
+print(visual_basic)
+
+programming_languages = [python, ruby, visual_basic]
+print(programming_languages)
+
+print("The dynamically typed languages are:")
+for programming_language in programming_languages:
+    if programming_language.is_dynamic():
+        print(programming_language.name)
