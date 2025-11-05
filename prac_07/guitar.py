609e8c7e8f42d6dc0b05626d1be663fa2aaf04b6
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-11-05 15:03:00 +1000

    Add guitar class from prac 6

---
+++ prac_07/guitar.py
@@ -0,0 +1,28 @@
+"""Guitar Class"""
+
+CURRENT_YEAR = 2025
+VINTAGE_THRESHOLD = 50
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a guitar."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a formatted string of name, year and cost."""
+        return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
+
+    def __repr__(self):
+        """Return a formatted string of name, year and cost."""
+        return str(self)
+
+    def get_age(self):
+        """Get age based on current year and the year guitar was created."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Check if guitar is vintage based on age."""
+        return Guitar.get_age(self) >= VINTAGE_THRESHOLD
