d063555ef95e0abfd8026b806a4ee0cee3e4c423
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-11-08 17:11:33 +1000

    Implement load functionality and project object class

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,6 +4,8 @@ Estimated Time: 70 minutes
 Actual Time: ??
 """
 
+from project import Project
+
 MENU = """- (L)oad projects  
 - (S)ave projects  
 - (D)isplay projects  
@@ -11,15 +13,20 @@ MENU = """- (L)oad projects
 - (A)dd new project  
 - (U)pdate project
 - (Q)uit"""
+DEFAULT_FILE = "projects.txt"
 
 
 def main():
     print("Welcome to Pythonic Project Management")
+    projects = load_projects(DEFAULT_FILE)
+    print(f"Loaded {len(projects)} projects from {DEFAULT_FILE}")
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
-            pass
+            filename = input("Filename: ")
+            load_projects(filename)
+            print(f"Loaded {len(projects)} projects from {filename}")
         elif choice == "S":
             pass
         elif choice == "D":
@@ -35,6 +42,17 @@ def main():
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you for using custom-built project management software.")
+
+
+def load_projects(filename):
+    projects = []
+    with open(filename, "r") as in_file:
+        in_file.readline()  # Skip data header
+        for line in in_file:
+            parts = line.strip().split("\t")
+            project = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4]))
+            projects.append(project)
+    return projects
 
 
 if __name__ == "__main__":
