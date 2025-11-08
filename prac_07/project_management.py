be97a888a9979d12924cd95663eef02fc933a4c4
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-11-08 16:09:39 +1000

    Add module docstring and menu for project management program

---
+++ prac_07/project_management.py
@@ -0,0 +1,41 @@
+"""
+Project Management Program
+Estimated Time: 70 minutes
+Actual Time: ??
+"""
+
+MENU = """- (L)oad projects  
+- (S)ave projects  
+- (D)isplay projects  
+- (F)ilter projects by date
+- (A)dd new project  
+- (U)pdate project
+- (Q)uit"""
+
+
+def main():
+    print("Welcome to Pythonic Project Management")
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            pass
+        elif choice == "S":
+            pass
+        elif choice == "D":
+            pass
+        elif choice == "F":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you for using custom-built project management software.")
+
+
+if __name__ == "__main__":
+    main()
