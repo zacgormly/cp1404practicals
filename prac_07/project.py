7a4e208161375599639958c85636704f1e3c7811
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-11-08 17:11:51 +1000

    Add project object class

---
+++ prac_07/project.py
@@ -0,0 +1,22 @@
+"""Project Class"""
+
+
+class Project:
+    """Create class representation of project object."""
+
+    def __init__(self, name, start_date, priority, cost, completion_percentage):
+        """Initialise a project."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost = cost
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        """Return a formatted string of name, start date, priority, estimate cost and completion."""
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
+                f"estimate: ${self.cost:.2f}, completion: {self.completion_percentage}%")
+
+    def __repr__(self):
+        """Return a formatted string of name, start date, priority, estimate cost and completion."""
+        return str(self)
