b0da4ef154597f655971939750fc9ff696d0de61
Zac Gormly <zac.gormly@my.jcu.edu.au>
2025-11-12 00:59:58 +1000

    Complete box layout GUI

--- prac_08/KivyDemos-master/box_layout_demo.py
+++ prac_08/KivyDemos-master/box_layout_demo.py
@@ -8,5 +8,14 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print("greet")
+        print("test")
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        self.root.ids.input_name.text = ""
+        self.root.ids.output_label.text = ""
+
 
 BoxLayoutDemo().run()
